0d9e9719f81d98b71b2e16269d8eabb570adba0a
Arun Kumar G <2632288-arung86@users.noreply.gitlab.com>
2020-10-07 07:15:42 +0530

    day2 reactjs changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import Button from "./Button";
+import { FButton, CButton } from "./Button";
+
+const Submit = <button>Hello !</button>;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3> Welcome !</h3>
+        <FButton />
+        <CButton />
+        {Submit}
       </header>
     </div>
   );
---
+++ src/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Submit = <button>Hello !</button>;
+
+function FButton() {
+  return <div>Click Me!</div>;
+}
+
+class CButton extends React.Component {
+  render() {
+    return <div>Click Me Class!</div>;
+  }
+}
+
+export { CButton, FButton };
+
+// function component
+// class component - ES6 class
+
+// element
