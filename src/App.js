1233879ac8761b5c8f90f3c4786e85f167d847bc
Arun Kumar G <2632288-arung86@users.noreply.gitlab.com>
2020-10-10 18:25:58 +0530

    integration with graphql end point

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+
+import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4001/graphql",
+  cache: new InMemoryCache(),
+});
+
+const GET_ROLES = gql`
+  {
+    roles {
+      id
+      roleName
+    }
+  }
+`;
+
+function Roles() {
+  const { loading, error, data } = useQuery(GET_ROLES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.roles.map(({ id, roleName }) => (
+    <div key={id}>
+      <p>
+        {id}: {roleName}
+      </p>
+    </div>
+  ));
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        <Roles />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+/*client
+  .query({
+    query: gql`
+      query getRoles {
+        roles {
+          id
+          roleName
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello Graphql</h2>
     </div>
   );
 }
-
+*/
 export default App;
