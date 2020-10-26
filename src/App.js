ebd219f82373ba184e680e3f2585d4c39b9fbb2d
vinayveerappaji <vinayveerappaji@gmail.com>
2020-10-26 22:36:31 +0530

    a simple axios post example added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./Posts";
+import PostForm from "./PostForm";
 
 function App() {
   return (
     <div className="App">
-      <Posts></Posts>
+      {/* <Posts/> */}
+      <PostForm />
     </div>
   );
 }
