f06fce67cc6ab758c28317566b4a8132c813bad8
Arun Kumar G <2632288-arung86@users.noreply.gitlab.com>
2020-11-12 09:45:21 +0530

    add chrome devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,13 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import reducer from "./reducer";
 import postsReducer from "./posts/reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({ user: reducer, post: postsReducer });
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk))
+);
 
 export default store;
